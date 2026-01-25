84786bceb14abe67a70026dc68b224e32b1abc83
elziodzaya <46269172+elziodzaya@users.noreply.github.com>
2026-01-26 06:08:46 +0700

    Add transcription function using Whisper model

--- utils/whisper.py
+++ utils/whisper.py
@@ -1,1 +1,22 @@
+import whisper
+
+model = whisper.load_model("base")
+
+def transcribe(video_path):
+    result = model.transcribe(
+        video_path,
+        word_timestamps=True
+    )
+
+    words = []
+    for seg in result["segments"]:
+        for w in seg["words"]:
+            words.append({
+                "word": w["word"].strip(),
+                "start": w["start"],
+                "end": w["end"]
+            })
+
+    return words
+
 
