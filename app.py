549bc00dc0d37261e3d44587fffe5e788cbab3c9
elziodzaya <46269172+elziodzaya@users.noreply.github.com>
2026-01-28 14:04:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import os
+os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
+os.environ["STREAMLIT_SERVER_PORT"] = "7860"
+os.environ["STREAMLIT_SERVER_ADDRESS"] = "0.0.0.0"
+
 import streamlit as st
 
 # ===============================
